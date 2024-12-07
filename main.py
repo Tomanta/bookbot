1de469a94010274fc6176a2dfb4c5d96afc5b62c
Tomanta <bzednick@gmail.com>
2024-12-07 13:37:43 -0600

    Added report function, refactored to add main entry

--- main.py
+++ main.py
@@ -12,8 +12,31 @@ def get_character_frequency(text: str):
     
     return char_frequency
 
-with open("books/frankenstein.txt") as f:
-    file_contents = f.read()
+def print_report(file_path: str, word_count: str, char_count: dict):
+    print(f"--- Begin report of {file_path} ---")
+    print(f"{word_count} words found in the document")
+    print("")
+    alpha_chars = "abcdefghijklmnopqrstuvwxyz"
+    
+    for k in char_count:
+        if k in alpha_chars:
+            frequency = char_count[k]
+            print(f"The '{k}' chracter was found {frequency} times")
+
+def get_file_contents(file_path: str) -> str:
+    with open(file_path) as f:
+        file_contents = f.read()
+    
+    return file_contents
+
+def main():
+    file_path = "books/frankenstein.txt"
+
+    file_contents = get_file_contents(file_path)
     word_count = get_count_words(file_contents)
     char_frequency = get_character_frequency(file_contents)
-    print(f"{char_frequency}")
+    
+    print_report(file_path, word_count, char_frequency)
+
+if __name__ == "__main__":
+    main()
